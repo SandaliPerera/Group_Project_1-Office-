843a90ce6e73576ff05e5ba9d56517ad1a5854f2
Sandali Hasara Perera <56555623+SandaliPerera@users.noreply.github.com>
2020-11-24 23:05:21 +0530

    Add files via upload

---
+++ o_alCsv.php
@@ -0,0 +1,70 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['userType']) && !isset($_SESSION['userID'])){
+        $error = "Please Login!";
+        header('Location: ../common/loginFile.php?error='.$error);
+    }
+    else if($_SESSION['userType'] != 'officer'){
+        header('Location: ../common/error.html');
+    }
+    else{      
+      $dutyID = array();
+      $dutyID = $_SESSION['dutyID'];
+
+      if (!in_array("d2", $dutyID)) {
+         header('Location: o_dashboard.php');
+        }
+	?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Add G.C.E. A/L Examination Results</title>
+    <link type="text/css" rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/register.css " type="text/css">
+    <script src="../js/jquery-1.9.1.min.js"></script>
+    <script src="../js/pop.js"></script>
+    <script src="../js/nav.js"></script>
+</head>
+
+<body>
+    <div id="nav"></div>
+
+    </div>
+    <div class="content">
+        <div class="regcontainer">
+            <form action="../php/register.php" method="POST">
+                <h1>Add G.C.E. A/L Examination Results</h1>
+                <hr>
+
+                <label for="examID"><b>Exam ID</b></label>
+                <input type="text" value="ALS123" name="examID" readonly>
+
+                <label for="olExamYear"><b>Enter Exam Year</b></label>
+                <input type="text" value="2016" name="olExamYear" readonly>
+
+                <label for="olexamName"><b>Exam Name</b></label>
+                <input type="text" value="G.C.E. A/L Examination - 2016" name="olexamName" readonly>
+
+                <label for="myFile"><b>Enter CSV File</b></label>
+                <input type="file" id="myFile" name="filename" class="nextpgbtn" required></br>
+
+                <button type="submit" class="registerbtn">Save</button>
+                <a href="o_viewAl.php" class="cancel-btn">Cancel</a>
+                <hr>
+
+            </form>
+
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
+
+<?php } ?>
