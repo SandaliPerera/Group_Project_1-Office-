e62eb5784fb6a27ac67e0874f1007cb44a70b454
Sandali Hasara Perera <56555623+SandaliPerera@users.noreply.github.com>
2020-11-24 23:05:42 +0530

    Add files via upload

--- o_scholCsv.php
+++ o_scholCsv.php
@@ -1,21 +1,40 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['userType']) && !isset($_SESSION['userID'])){
+        $error = "Please Login!";
+        header('Location: ../common/loginFile.php?error='.$error);
+    }
+    else if($_SESSION['userType'] != 'officer'){
+        header('Location: ../common/error.html');
+    }
+    else{      
+      $dutyID = array();
+      $dutyID = $_SESSION['dutyID'];
+
+      if (!in_array("d2", $dutyID)) {
+         header('Location: o_dashboard.php');
+        }
+    ?>
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Add Scholarship Results</title>
-    <link rel="stylesheet" href="../css/register.css " type="text/css">
     <link type="text/css" rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/register.css " type="text/css">
     <script src="../js/jquery-1.9.1.min.js"></script>
     <script src="../js/pop.js"></script>
     <script src="../js/nav.js"></script>
 </head>
 
 <body>
-    <div id="nav"></div>
+    <div id="officeNav"></div>
 
     <div class="content">
-        <div class="container">
+        <div class="regcontainer">
             <form action="../php/register.php" method="POST">
                 <h1>Add Grade 5 Scholarship Exam Results</h1>
                 <hr>
@@ -29,20 +48,13 @@
                 <label for="examName"><b>Exam Name</b></label>
                 <input type="text" value="Grade 5 Scholarship Exam - 2016" name="examName" readonly>
 
+                <label for="myFile"><b>Enter CSV File</b></label>
+                <input type="file" id="myFile" name="filename" class="nextpgbtn" required></br>
 
-                <br>
+                <button type="submit" class="registerbtn">Save</button>
+                <a href="o_viewSchol.php" class="cancel-btn">Cancel</a>
 
-                <p>Click on the "Choose File" button to upload a CSV file :</p>
-
-                <form>
-
-                    <input type="file" id="myFile" name="filename" class="nextpgbtn" required></br>
-
-                    <hr>
-
-                    <button type="submit" class="registerbtn">Save</button>
-
-                </form>
+            </form>
 
         </div>
 
@@ -52,3 +64,5 @@
 </body>
 
 </html>
+
+<?php } ?>
